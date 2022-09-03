755fc158d6f33a1d4bbb2fcb7d37b36170efdee4
Rabbi Ahmed Limon <rabbiahemed>
2022-09-04 05:07:37 +0600

    modal done

--- js/index.js
+++ js/index.js
@@ -90,30 +90,6 @@ const displayCategoryDetails = (categories) => {
   //
   else {
     toggleViewSection(true);
-    /* const viewSection = document.getElementById("view-section");
-    const div = document.createElement("div");
-    viewSection.innerHTML = ``;
-    div.classList.add("d-flex");
-    div.classList.add("justify-content-around");
-    div.classList.add("align-items-center");
-    div.innerHTML = `
-    <div class="dropdown">
-    <button class="btn btn-secondary dropdown-bs-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      Default
-    </button>
-    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-      <a class="dropdown-item" href="#">Action</a>
-      <a class="dropdown-item" href="#">Another action</a>
-      <a class="dropdown-item" href="#">Something else here</a>
-    </div
-    </div>
-    <div class="d-flex">
-    <button type="button" class="btn btn-primary mx-1 fw-bold">Today's Pick</button>
-    <button type="button" class="btn btn-outline-primary mx-1 fw-bold">Trending</button>
-    </div>
-    
-    `;
-    viewSection.appendChild(div); */
     for (const category of categories) {
       const div = document.createElement("div");
       div.classList.add("mb-3");
@@ -133,11 +109,11 @@ const displayCategoryDetails = (categories) => {
         category.author.img
       }" class=" author-img rounded-circle" alt="author..."></img>
       <span class="card-text fst-italic"><small class="text-muted">${
-        category?.author?.name
+        category.author.name ? category.author.name : "No data available"
       }</small></span>
       </div>
       <div><span class="card-text fst-italic"><small class="text-muted">Views:${
-        category.total_view
+        category.total_view ? category.total_view : "No data available"
       }</small></span></div>
       <div><button onclick="loadNewsDetail('${
         category._id
@@ -167,21 +143,44 @@ const displayNewsDetails = (news) => {
   modalBody.innerHTML = ``;
   const div = document.createElement("div");
   div.classList.add("modal-content");
+  div.classList.add("bg-light");
   div.innerHTML = `
 
-      <div class="modal-header">
-        <h5 class="" id="exampleModalLongTitle">${news.title}</h5>
-        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+      <div class="modal-header bg-info">
+      <h5 class="modal-title bg-info" id="exampleModalLongTitle">News Details</h5>
+        <button type="button" class="close btn-danger" data-bs-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <p class="fw-bold">${news.details}</p>
-        <p></p>
+      <img src="${news.image_url}" class=" img-fluid w-100" alt=""></img>
+      <h5 class="my-3" id="exampleModalLongTitle">${news.title}</h5>
+      
+      <img src="${
+        news.author.img
+      }" class=" author-img rounded-circle my-2" alt="author..."></img>
+      <span class="fst-italic"><small class="text-muted my-2">Author : ${
+        news.author.name ? news.author.name : "No data available"
+      }</small></span>
+      <div class="d-flex justify-content-around align-items-center"> 
+      
+      <p class="fst-italic">Total Views : ${
+        news.total_view ? news.total_view : "No data available"
+      }</p>
         
+        <p class="fst-italic">Rating : ${news.rating.number} </p>
+        <p class="ms-2 fst-italic">Comment : ${news.rating.badge}</p>
+        </div>
+        <p class="fw-bold fst-italic">${news.details}</p>
+        
+        <p><span class="fst-italic">Publishing Date : </span> ${
+          news.author.published_date
+            ? news.author.published_date
+            : "Publishing date is not available"
+        }</p>  
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Close</button>
       </div>
       `;
   modalBody.appendChild(div);
