e6cd41adc5e656839f12be5261d096c9761964c2
Rabbi Ahmed Limon <rabbiahemed>
2022-09-03 13:42:34 +0600

    no data found implemented

--- js/index.js
+++ js/index.js
@@ -1,35 +1,89 @@
+// Load Categories
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/news/categories")
     .then((res) => res.json())
     .then((data) => showCategories(data.data.news_category));
 };
-
+// Show Categories
 const showCategories = (categories) => {
   const categoriesSection = document.getElementById("categories-container");
 
   categories.filter((category) => {
-    // console.log(category.category_id);
     const div = document.createElement("div");
     div.innerHTML = `
     <div onclick="loadCategoryDetail(${category.category_id})">
 
-                <p class="mx-4">${category.category_name}</p>
+                <p class="mx-4 fw-bold">${category.category_name}</p>
             
         </div>`;
     categoriesSection.appendChild(div);
   });
 };
 
+// Load Categories Details
 const loadCategoryDetail = (category_id) => {
-  //   console.log(category_id);
   const url = `https://openapi.programming-hero.com/api/news/category/0${category_id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayResult(data.data));
-  //   .then((data) => displayDetails(data.data[0]));
+    .then((data) => displayCategoryDetails(data.data))
+    .catch((error) => console.log(error));
 };
 
-const displayResult = (items) => {
+const displayCategoryDetails = (categories) => {
+  const noResult = document.getElementById("no-result");
+  // console.log(categories);
+
+  // console.log(typeof categories);
+
+  // show number of contents in that category
+  const lengthOfArray = [];
+  const arrayLength = categories.map((category) =>
+    lengthOfArray.push(category)
+  );
+  // console.log(arrayLength);
+  const resultNumber = document.getElementById("result-number");
+  resultNumber.innerHTML = ``;
+  const p = document.createElement("p");
+  p.innerHTML = `Total Number of Matching Content : ${arrayLength.length}`;
+  resultNumber.appendChild(p);
+  //
+  const categoryDetailsContainer = document.getElementById(
+    "category-details-container"
+  );
+  categoryDetailsContainer.innerHTML = "";
+  // check if category has data inside
+  if (categories.length == 0) {
+    resultNumber.innerHTML = ``;
+    categoryDetailsContainer.classList.add("text-center");
+    categoryDetailsContainer.textContent = "No Results Found";
+    noResult.innerHTML = ``;
+  } else {
+    console.log("found");
+    for (const category of categories) {
+      // No Result Found
+
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
+          <div onclick="loadDetail(${category._id})" class="card h-100">
+                  <div class="card-body">
+                      <h5 class="card-title">${category.title}</h5>
+                      <p class="card-text">T${category.details.slice(
+                        0,
+                        150
+                      )}</p>
+                  </div>
+              </div>`;
+      categoryDetailsContainer.appendChild(div);
+    }
+  }
+
+  //
+};
+
+// Display Category Result
+
+/* const displayResult = (items) => {
   const categoryDetailsContainer = document.getElementById(
     "category-details-container"
   );
@@ -41,8 +95,8 @@ const displayResult = (items) => {
     categoryDetailsContainer.textContent = "No Results Found";
     noResult.innerHTML = ``;
   }
+
   items.forEach((item) => {
-    console.log(items.length);
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
@@ -54,6 +108,6 @@ const displayResult = (items) => {
             </div>`;
     categoryDetailsContainer.appendChild(div);
   });
-};
+}; */
 
 loadCategories();
