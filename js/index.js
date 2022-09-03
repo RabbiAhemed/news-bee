887a4af44e1d743ae8ee1a4f014adbc53b627fe7
Rabbi Ahmed Limon <rabbiahemed>
2022-09-03 15:36:11 +0600

    display all news in a category done

--- js/index.js
+++ js/index.js
@@ -31,83 +31,85 @@ const loadCategoryDetail = (category_id) => {
 
 const displayCategoryDetails = (categories) => {
   const noResult = document.getElementById("no-result");
-  // console.log(categories);
-
-  // console.log(typeof categories);
 
   // show number of contents in that category
   const lengthOfArray = [];
   const arrayLength = categories.map((category) =>
     lengthOfArray.push(category)
   );
-  // console.log(arrayLength);
   const resultNumber = document.getElementById("result-number");
   resultNumber.innerHTML = ``;
   const p = document.createElement("p");
   p.innerHTML = `Total Number of Matching Content : ${arrayLength.length}`;
   resultNumber.appendChild(p);
   //
+  // Get Category Details Container
   const categoryDetailsContainer = document.getElementById(
     "category-details-container"
   );
+
   categoryDetailsContainer.innerHTML = "";
-  // check if category has data inside
+  // check if category has data inside and show no data found
   if (categories.length == 0) {
     resultNumber.innerHTML = ``;
     categoryDetailsContainer.classList.add("text-center");
     categoryDetailsContainer.textContent = "No Results Found";
     noResult.innerHTML = ``;
-  } else {
-    console.log("found");
+  }
+  //
+  else {
     for (const category of categories) {
-      // No Result Found
-
       const div = document.createElement("div");
-      div.classList.add("col");
+      div.classList.add("mb-3");
+      div.classList.add("card");
       div.innerHTML = `
-          <div onclick="loadDetail(${category._id})" class="card h-100">
-                  <div class="card-body">
-                      <h5 class="card-title">${category.title}</h5>
-                      <p class="card-text">T${category.details.slice(
-                        0,
-                        150
-                      )}</p>
-                  </div>
-              </div>`;
+  <div class="row g-0 p-2 ">
+  <div class="col-md-4">
+    <img src="${category.thumbnail_url}" class="img-fluid " alt="...">
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+      <h5 class="card-title">${category.title}</h5>
+      <p class="card-text">${category.details.slice(0, 150).concat("...")}</p>
+      <div class="card-bottom">
+      <div class="author-info">
+      <img src="${
+        category.author.img
+      }" class=" author-img rounded-circle" alt="author..."></img>
+      <span class="card-text fst-italic"><small class="text-muted">${
+        category.author.name
+      }</small></span>
+      </div>
+      
+      <div><span class="card-text fst-italic"><small class="text-muted">Views:${
+        category.total_view
+      }</small></span></div>
+      
+      <div><button type="button" class="btn btn-info">Info</button>
+      </div>
+      
+      
+      </div>
+     
+     
+      </div>
+
+  </div>
+</div>
+
+  `;
+
       categoryDetailsContainer.appendChild(div);
     }
   }
-
-  //
 };
 
-// Display Category Result
-
-/* const displayResult = (items) => {
-  const categoryDetailsContainer = document.getElementById(
-    "category-details-container"
-  );
-  // clear page/result after one search and for next search
-  categoryDetailsContainer.innerHTML = "";
-
-  if (items.length === 0) {
-    const noResult = document.getElementById("no-result");
-    categoryDetailsContainer.textContent = "No Results Found";
-    noResult.innerHTML = ``;
-  }
-
-  items.forEach((item) => {
-    const div = document.createElement("div");
-    div.classList.add("col");
-    div.innerHTML = `
-        <div onclick="loadDetail(${item._id})" class="card h-100">
-                <div class="card-body">
-                    <h5 class="card-title">${item.title}</h5>
-                    <p class="card-text">T${item.details.slice(0, 150)}</p>
-                </div>
-            </div>`;
-    categoryDetailsContainer.appendChild(div);
-  });
-}; */
+const loadNewsDetail = (news_id) => {
+  const url = `https://openapi.programming-hero.com/api/news/{news_id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
+loadNewsDetail();
 
 loadCategories();
